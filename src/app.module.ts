5ea00645cf5b1353ec917717f93891b73fbc11dc
Sumit Santhosh Nair <sumitnair731@gmail.com>
2026-03-14 11:01:55 +0000

    feat: scaffold meeting module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MeetingModule } from './meeting/meeting.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env',
-  }),
+  }), MeetingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/meeting/meeting.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MeetingService } from './meeting.service';
+import { MeetingController } from './meeting.controller';
+
+@Module({
+  providers: [MeetingService],
+  controllers: [MeetingController]
+})
+export class MeetingModule {}
